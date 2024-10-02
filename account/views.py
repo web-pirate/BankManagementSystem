084e931b8f621d55b10c57bdd56ccff2df38cf98
Manish <manishsinghdewas@gmail.com>
2024-10-02 13:27:52 +0530

    UI:HomePage and ServicePage

---
+++ account/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(requests):
+    context = {
+        'nav_links': [
+            {'name': 'Home', 'url': 'home'},
+            {'name': 'Service', 'url': 'service'},
+        ]
+    }
+    return render(requests, 'home.html', context)
+
+
+def service(requests):
+    return render(requests,  'service.html')
